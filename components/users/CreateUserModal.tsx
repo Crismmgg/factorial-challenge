8cb1be9abc90a731224e4701e04c83420f40780e
crismmgg <cristinammgg@gmail.com>
2023-03-14 17:40:11 +0100

    fix: allow close modal when mobile

--- components/users/CreateUserModal.tsx
+++ components/users/CreateUserModal.tsx
@@ -30,7 +30,7 @@ export default function CreateUserModal({
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width: 600,
+    width: { xs: 350, sm: 600 },
     bgcolor: "background.paper",
     boxShadow: 24,
     p: 2,
